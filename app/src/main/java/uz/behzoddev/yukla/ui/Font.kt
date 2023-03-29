662b1a7f390cf659827d4a2366241019dce0be1a
Behzod Halil <behzoddev@gmail.com>
2023-03-29 14:00:29 +0900

    Add fonts

---
+++ app/src/main/java/uz/behzoddev/yukla/ui/Font.kt
@@ -0,0 +1,12 @@
+package uz.behzoddev.yukla.ui
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import uz.behzoddev.yukla.R
+
+val poppins = FontFamily(
+  Font(R.font.poppins_regular, weight = FontWeight.Normal, style = FontStyle.Normal),
+  Font(R.font.poppins_medium, weight = FontWeight.Medium, style = FontStyle.Normal)
+)
