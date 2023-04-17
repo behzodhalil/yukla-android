56f682ae636ec4f62cc32a390d358fe057368077
Behzod Halil <behzoddev@gmail.com>
2023-04-17 18:05:54 +0900

    Add video type

---
+++ core/ui-components/src/main/java/io/behzod/uicomponents/VideoType.kt
@@ -0,0 +1,13 @@
+package io.behzod.uicomponents
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun VideoType(
+  text: String,
+  videoSize: String,
+  modifier: Modifier = Modifier,
+) {
+
+}
