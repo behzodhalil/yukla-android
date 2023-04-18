59936bcd16f5b9ee5a2c7e51ad532a0b7e694ab6
Behzod Halil <behzoddev@gmail.com>
2023-04-18 12:31:36 +0900

    Update gradle & material3

--- features/downloader/build.gradle.kts
+++ features/downloader/build.gradle.kts
@@ -1,44 +1,59 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+  id("yukla.android.library")
 }
 
 android {
-    namespace = "io.behzod.downloader"
-    compileSdk = 33
+  namespace = "io.behzod.downloader"
 
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 33
+  defaultConfig {
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+    vectorDrawables {
+      useSupportLibrary = true
+    }
+  }
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.4.1"
+  }
+  packagingOptions {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  implementation(libs.androidx.core)
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.tooling.preview)
+  implementation(libs.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation("androidx.compose.material:material:1.4.1")
+  implementation("androidx.compose.material3:material3:1.0.1")
+  implementation("androidx.compose.material:material-icons-extended:1.4.1")
+  debugImplementation("androidx.compose.ui:ui-tooling:1.4.1")
+  testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("androidx.test.ext:junit:1.1.5")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  implementation(project(":core:ui-components"))
 }
