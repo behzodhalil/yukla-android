96be47ba924b575a9d6b9f17bec3947b0e76d328
Behzod Halil <behzoddev@gmail.com>
2023-04-20 14:24:11 +0900

    Add downloader screen

--- features/downloader/src/main/java/io/behzod/downloader/Downloader.kt
+++ features/downloader/src/main/java/io/behzod/downloader/Downloader.kt
@@ -1,12 +1,15 @@
 package io.behzod.downloader
 
+import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cloud
 import androidx.compose.material.icons.filled.TrackChanges
@@ -15,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.behzod.uicomponents.PasteButton
 import io.behzod.uicomponents.TopButton
 import io.behzod.uicomponents.UrlField
 
@@ -38,13 +42,23 @@ fun Downloader(
         TopButton(imageVector = Icons.Default.Cloud)
 
       }
+      Row(
+        modifier = modifier.padding(top = 24.dp)
+      ) {
+        UrlField(
+          url = "",
+          onValueChanged = {},
+          onDone = {}
+        )
 
-      UrlField(
-        modifier = modifier.padding(top = 24.dp),
-        url = "",
-        onValueChanged = {},
-        onDone = {}
-      )
+        Spacer(modifier = modifier.width(16.dp))
+
+        PasteButton(
+        ) {
+
+        }
+      }
+
     }
 
   }
