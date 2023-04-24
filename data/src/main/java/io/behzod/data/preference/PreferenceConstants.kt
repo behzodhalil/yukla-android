f0d8c384414fc3c5312600bee25cd9aa2e2a6d1b
Behzod Halil <behzoddev@gmail.com>
2023-04-24 17:50:56 +0900

    Add PreferenceConstants.kt

---
+++ data/src/main/java/io/behzod/data/preference/PreferenceConstants.kt
@@ -0,0 +1,84 @@
+package io.behzod.data.preference
+
+const val CUSTOM_COMMAND = "custom_command"
+const val CONCURRENT = "concurrent_fragments"
+const val EXTRACT_AUDIO = "extract_audio"
+const val THUMBNAIL = "create_thumbnail"
+const val YT_DLP = "yt-dlp_init"
+const val YT_DLP_NIGHTLY = "yt-dlp_nightly"
+const val YT_DLP_UPDATE = "yt-dlp_auto_update"
+const val DEBUG = "debug"
+const val CONFIGURE = "configure"
+const val DARK_THEME_VALUE = "dark_theme_value"
+const val AUDIO_CONVERT = "audio_convert"
+const val AUDIO_CONVERSION_FORMAT = "audio_convert_format"
+const val AUDIO_FORMAT = "audio_format_preferred"
+const val AUDIO_QUALITY = "audio_quality"
+const val VIDEO_FORMAT = "video_format"
+const val VIDEO_QUALITY = "quality"
+
+const val FORMAT_SORTING = "format_sorting"
+const val SORTING_FIELDS = "sorting_fields"
+
+const val WELCOME_DIALOG = "welcome_dialog"
+const val VIDEO_DIRECTORY = "download_dir"
+const val AUDIO_DIRECTORY = "audio_dir"
+const val SDCARD_DOWNLOAD = "sdcard_download"
+const val SDCARD_URI = "sd_card_uri"
+const val SUBDIRECTORY = "sub-directory"
+const val PLAYLIST = "playlist"
+const val LANGUAGE = "language"
+const val NOTIFICATION = "notification"
+private const val THEME_COLOR = "theme_color"
+const val PALETTE_STYLE = "palette_style"
+const val CUSTOM_PATH = "custom_path"
+const val OUTPUT_PATH_TEMPLATE = "path_template"
+const val SUBTITLE = "subtitle"
+const val EMBED_SUBTITLE = "embed_subtitle"
+const val KEEP_SUBTITLE_FILES = "keep_subtitle"
+const val SUBTITLE_LANGUAGE = "sub_lang"
+const val AUTO_SUBTITLE = "auto_subtitle"
+
+const val TEMPLATE_ID = "template_id"
+const val MAX_FILE_SIZE = "max_file_size"
+const val SPONSORBLOCK = "sponsorblock"
+const val SPONSORBLOCK_CATEGORIES = "sponsorblock_categories"
+const val ARIA2C = "aria2c"
+const val COOKIES = "cookies"
+const val AUTO_UPDATE = "auto_update"
+const val UPDATE_CHANNEL = "update_channel"
+const val PRIVATE_MODE = "private_mode"
+private const val DYNAMIC_COLOR = "dynamic_color"
+const val CELLULAR_DOWNLOAD = "cellular_download"
+const val RATE_LIMIT = "rate_limit"
+const val MAX_RATE = "max_rate"
+private const val HIGH_CONTRAST = "high_contrast"
+const val DISABLE_PREVIEW = "disable_preview"
+const val PRIVATE_DIRECTORY = "private_directory"
+const val CROP_ARTWORK = "crop_artwork"
+const val EMBED_THUMBNAIL = "embed_thumbnail"
+const val FORMAT_SELECTION = "format_selection"
+const val VIDEO_CLIP = "video_clip"
+const val TEMP_DIRECTORY = "temp_dir"
+
+const val DEFAULT = 0
+const val NOT_SPECIFIED = 0
+const val SYSTEM_DEFAULT = 0
+
+const val STABLE = 0
+const val PRE_RELEASE = 1
+
+const val OPUS = 1
+const val M4A = 2
+
+const val MP4 = 1
+const val VP9 = 2
+const val AV1 = 3
+
+const val CONVERT_MP3 = 0
+const val CONVERT_M4A = 1
+
+const val HIGH = 1
+const val MEDIUM = 2
+const val LOW = 3
+const val ULTRA_LOW = 4
